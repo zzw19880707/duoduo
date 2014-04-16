a5bc010c849630f6b7792ff4dca2a7f693b88bf6
zzw19880707 <zzw414851474@qq.com>
2014-04-16 15:34:14 +0800

    商品信息页

---
+++ duoduo1.0.1/duoduo/CourseViewController.h
@@ -0,0 +1,15 @@
+//
+//  CourseViewController.h
+//  duoduo
+//
+//  Created by tenyea on 14-4-11.
+//  Copyright (c) 2014年 zzw. All rights reserved.
+//
+
+#import "TenyeaBaseViewController.h"
+
+@interface CourseViewController : TenyeaBaseViewController
+
+@property (nonatomic ,strong) NSString *CourseID;
+
+@end
---
+++ duoduo1.0.1/duoduo/CourseViewController.m
@@ -0,0 +1,39 @@
+//
+//  CourseViewController.m
+//  duoduo
+//
+//  Created by tenyea on 14-4-11.
+//  Copyright (c) 2014年 zzw. All rights reserved.
+//
+
+#import "CourseViewController.h"
+
+@interface CourseViewController ()
+
+@end
+
+@implementation CourseViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+        self.view.backgroundColor = [UIColor whiteColor];
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
